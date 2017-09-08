b919f8d709bd1ea56e86fce876e49259e1e6935a
James Kyle <me@thejameskyle.com>
2017-09-08 18:42:13 +1000

    update to pretty-format2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // @flow
 'use strict';
 
-const prettyFormat = require('pretty-format');
+const prettyFormat = require('pretty-format2');
 const prettyFormatAST = require('pretty-format-ast');
 
 function printAST(val /*: mixed */, highlight /*: boolean */ = false) {
